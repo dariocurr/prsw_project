456773c34592fd7acfe7cec3e848c1143e79badd
salvatore <salvatore@LAPTOP-PUVR5VPL>
2020-11-04 15:55:01 +0100

    created rmi server

--- CarRental/src/car_rental/ICarRental.java
+++ CarRental/src/car_rental/ICarRental.java
@@ -1,14 +1,15 @@
 package car_rental;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import common.IRenter;
 import common.IVehicle;
 import common.Observable;
 import common.Rent;
 
-public interface ICarRental extends Observable {
+public interface ICarRental extends Observable, Remote {
 	
-	public Rent rentVehicle(IRenter renter, IVehicle vehicle, String startDate, String endDate);
-	
-	public void returnVehicle(Rent rent);
+	public Rent rentVehicle(IRenter renter, IVehicle vehicle, String startDate, String endDate) throws RemoteException;
+	public void returnVehicle(Rent rent) throws RemoteException;
 	
 }
