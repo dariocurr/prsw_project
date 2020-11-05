9eccf3f0dc44d81dc9159e41d05becc5a6885a3f
dariocurr <dariocurr@gmail.com>
2020-11-05 15:49:59 +0100

    add rentals for renter

--- CarRental/src/car_rental/Renter.java
+++ CarRental/src/car_rental/Renter.java
@@ -1,7 +1,10 @@
 package car_rental;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import common.IRent;
 import common.IRenter;
 
 public class Renter implements IRenter {
@@ -11,6 +14,7 @@ public class Renter implements IRenter {
 	private String email;
 	private String password;
 	private boolean discounted;
+	private List<IRent> rentals;
 	
 	
 	
@@ -24,6 +28,7 @@ public class Renter implements IRenter {
 		this.email = email;
 		this.password = password;
 		this.discounted = discounted;
+		this.rentals = new ArrayList<>();
 	}
 	
 	@Override
