ea20a25fb1ccab75bdf6effb1ce56294c7ba864e
dariocurr <dariocurr@gmail.com>
2020-11-06 18:32:07 +0100

    update client interfaces

--- Company/src/common/IRent.java
+++ Company/src/common/IRent.java
@@ -5,11 +5,17 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IRent extends Serializable {
-
+	
 	public IRenter getRenter();
 	
 	public IVehicle getVehicle();
 	
+	public String getStartDate();
+
 	public String getEndDate();
+
+	public int getDaysNumber();
+	
+	public double getPrice();
 	
 }
